aab47a4ae62075be350325f77763b1ab38666b56
ageobab <agbabiki@gmail.com>
2014-05-05 10:47:40 -0400

    pulled from heroku, fog update for unbroken serve

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
-    provider: ENV['FOG_PROVIDER'],
+    provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: ENV['FOG_REGION']
